08f32df0c7c31b793a1dd7c423306b81d31af942
xiang.li5 <xiang.li5@thoughtworks.com>
2023-03-07 11:12:54 +0800

    init

---
+++ src/main/java/com/lixiang/lxstorebackend/LxStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lixiang.lxstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxStoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lixiang/lxstorebackend/LxStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixiang.lxstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
