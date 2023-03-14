a2c493a057785557fed47afd3aec26341067c863
xiang.li5 <xiang.li5@thoughtworks.com>
2023-03-14 16:00:04 +0800

    fix: 添加创建时间和修改时间

--- src/main/java/com/lixiang/lxstorebackend/entity/BaseEntity.java
+++ src/main/java/com/lixiang/lxstorebackend/entity/BaseEntity.java
@@ -1,13 +1,40 @@
 package com.lixiang.lxstorebackend.entity;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
-@Data
+@Setter
+@Getter
+//@Data
+@Accessors(chain = true)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
     private String createdUser;
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_time")
+    @JsonIgnore
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createdTime;
+
     private String modifiedUser;
+
+    @LastModifiedDate
+    @Column(name = "modified_time")
+    @JsonIgnore
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date modifiedTime;
 }
